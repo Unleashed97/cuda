23553ea5204cf1204ec87cb1a3f34b935e315537
Nichkov Roman <nichkovroman@gmail.com>
2021-02-05 23:25:14 +0300

    burger menu fix: closing, highlighting links after click

--- src/js/menu-toggle.js
+++ src/js/menu-toggle.js
@@ -1,11 +1,24 @@
 let menuToggle = () =>{
     let btnToggle = document.querySelector('.nav-toggle');
     let menu = document.querySelector('.nav');
+    let navLinks = document.querySelectorAll('.nav__link');
     let header = document.querySelector('.header');
     btnToggle.addEventListener('click', () => {
         btnToggle.classList.toggle('active');
         menu.classList.toggle('active');
         document.body.classList.toggle('stop-scroll');
     });
+
+    navLinks.forEach(item =>{
+        item.addEventListener('click', () => {
+            navLinks.forEach(i =>{
+                i.classList.remove('active');
+            })
+            item.classList.add('active');
+            btnToggle.classList.remove('active');
+            menu.classList.remove('active');
+            document.body.classList.remove('stop-scroll')
+        })
+    })
 }
 menuToggle();
